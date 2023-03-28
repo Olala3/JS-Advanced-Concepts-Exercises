abcce9d1166bd7e965da698ae99aa9862b018d93
Olala <olasabbouh@gmail.com>
2023-03-28 22:58:58 +0300

    exercise 10

---
+++ 10.js
@@ -0,0 +1,9 @@
+const order = {};
+
+
+if (!order?.customer?.address?.city) {
+  console.log('City is required');
+}
+
+/* I did get the optional chaining but I'm not
+sure if I got the question right*/
